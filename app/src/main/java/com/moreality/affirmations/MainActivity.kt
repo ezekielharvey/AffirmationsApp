a3eff1a437ab06f822b932cdb8665a71b08ad0ca
ezekielharvey <52931938+ezekielharvey@users.noreply.github.com>
2022-06-05 14:03:02 -0400

    Update MainActivity.kt

--- app/src/main/java/com/moreality/affirmations/MainActivity.kt
+++ app/src/main/java/com/moreality/affirmations/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = ItemAdapter(this, myDataset)
 
         // Use this setting to improve performance if you know that changes
-        // in content do not change hte layout size of the RecyclerView
+        // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
     }
 }
