7bcb48f05ea49f5bd54494c8b5f2f4fa3de3df17
ezekielharvey <52931938+ezekielharvey@users.noreply.github.com>
2022-06-10 17:38:06 -0400

    Update MainActivity.kt

--- app/src/main/java/com/moreality/affirmations/MainActivity.kt
+++ app/src/main/java/com/moreality/affirmations/MainActivity.kt
@@ -17,9 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
-
-        // Use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
+        
         recyclerView.setHasFixedSize(true)
     }
 }
