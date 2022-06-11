d978eb1ddac28d6072e55d2e50a77a6ac8d34e91
ezekielharvey <52931938+ezekielharvey@users.noreply.github.com>
2022-06-11 09:59:08 -0400

    Update MainActivity.kt

--- app/src/main/java/com/moreality/affirmations/MainActivity.kt
+++ app/src/main/java/com/moreality/affirmations/MainActivity.kt
@@ -11,8 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Initialize data
+        
         val myDataset = Datasource().loadAffirmations()
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
